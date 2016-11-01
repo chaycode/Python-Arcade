73078701c27a891d20f02af4692097c7bc04d251
Anudle <AnuShrestha007@gmail.com>
2016-11-01 13:23:23 -0600

    detail page added

--- pythonArcade/landing/apps.py
+++ pythonArcade/landing/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class LandingpageConfig(AppConfig):
-    name = 'landingPage'
+class LandingConfig(AppConfig):
+    name = 'landing'
--- pythonArcade/landing/urls.py
+++ pythonArcade/landing/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
-# Create your views here.
+
 urlpatterns = [
+    #/
     url(r'^$', views.index, name='index'),
+
+    #/413
+    url(r'^(?P<game_id>[0-9]+)$', views.detail, name='detail' )
 ]
