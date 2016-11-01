ad81b8fad16212e38aa01ed69916122e3429bf81
Anudle <AnuShrestha007@gmail.com>
2016-11-01 15:35:48 -0600

    added more details

--- pythonArcade/landing/models.py
+++ pythonArcade/landing/models.py
@@ -4,7 +4,14 @@ from django.db import models
 
 class Game(models.Model):
     name = models.CharField(max_length=200)
-    upvotes = models.IntegerField()
+    descriptions = models.CharField(max_length=500)
     #
     def __str__(self):
         return self.name
+
+class Poll(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    good = models.IntegerField()
+    bad = models.IntegerField()
+    meh = models.IntegerField()
+    
