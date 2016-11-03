984069dd9693106721b4fc14ee4e0f419302059c
Chay <chay@LittleMac.local>
2016-11-03 14:49:53 -0600

    heroku

--- pythonArcade/settings.py
+++ pythonArcade/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'landing.apps.LandingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing.apps.LandingConfig'
+
 ]
 
 MIDDLEWARE = [
