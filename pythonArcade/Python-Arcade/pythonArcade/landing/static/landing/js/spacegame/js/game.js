3fe70bc4fccc3f4c3dac5440f77e157e4382e2ed
Chay <chay@LittleMac.local>
2016-11-03 13:58:49 -0600

    heroku

---
+++ pythonArcade/Python-Arcade/pythonArcade/landing/static/landing/js/spacegame/js/game.js
@@ -0,0 +1,161 @@
+var bullets;
+var enemyBullets;
+var fireRate = 100;
+var nextFire = 0;
+var firingTimer = 0;
+var score = 0;
+var scoreText;
+var lives = 3;
+var livesText;
+var enemySpawnSpeed1 = 5550;
+var enemySpawnSpeed2 = 1300;
+
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+  preload: preload,
+  create: create,
+  update: update
+});
+
+function preload(){
+  // game.load.image('bottom', 'js/platform.png')
+  game.load.image('line', '/static/landing/js/spacegame/js/linepiece.png')
+  game.load.image('bullet', '/static/landing/js/spacegame/js/missle.png');
+  game.load.image('ufo', '/static/landing/js/spacegame/js/ufo.png');
+  game.load.image('ufo_bullet', '/static/landing/js/spacegame/js/ufo_missle.png');
+  game.load.image('ship', '/static/landing/js/spacegame/js/ship.png');
+  game.load.image('city', '/static/landing/js/spacegame/js/cityscape.jpg')
+}
+
+function create(){
+  city = game.add.tileSprite(0 , 0, 4098 , 768, 'city')
+
+  scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+  livesText = game.add.text(600, 16, 'Lives: 3', { fontSize: '32px', fill: '#000' });
+
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.stage.backgroundColor = "#5a739b"
+
+  bullets = game.add.group();
+  bullets.enableBody = true;
+  bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+  bullets.createMultiple(150, 'bullet');
+  bullets.setAll('checkWorldBounds', true)
+  bullets.setAll('outOfBoundsKill', true)
+
+  enemyBullets = game.add.group();
+  enemyBullets.enableBody = true;
+  enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+  enemyBullets.createMultiple(150, 'ufo_bullet');
+  enemyBullets.setAll('checkWorldBounds', true)
+  enemyBullets.setAll('outOfBoundsKill', true)
+
+  player = game.add.sprite(32, game.world.height / 2, 'ship')
+  game.physics.arcade.enable(player);
+  player.body.collideWorldBounds = true;
+
+  enemies = game.add.group();
+  enemies.enableBody = true;
+  game.physics.arcade.enable(enemies);
+
+  createEnemy();
+}
+
+function enemyKill (player, enemies) {
+  enemies.kill();
+  score += 10;
+  scoreText.text = 'Score: ' + score;
+}
+
+function playerKill (player, enemies) {
+  player.kill();
+  lives -= 1;
+  livesText.text = 'Lives: ' + lives;
+}
+
+function bulletKill (enemy, bullet) {
+  bullet.kill();
+}
+
+function fire() {
+  if (game.time.now > nextFire && bullets.countDead() > 0)
+  {
+    nextFire = game.time.now + fireRate;
+    var bullet = bullets.getFirstDead();
+    bullet.reset(player.x + 60, player.y);
+    bullet.body.velocity.x = 200
+  }
+}
+
+function createEnemy() {
+  enemy = enemies.create(1000, game.world.randomY, 'ufo')
+  enemy.body.velocity.x = -150;
+  game.time.events.add(game.rnd.integerInRange(enemySpawnSpeed1, enemySpawnSpeed2), createEnemy);
+}
+
+function enemyFire() {
+  if (game.time.now > nextFire && enemyBullets.countDead() > 0)
+  {
+    nextFire = game.time.now + fireRate;
+    var enemyBullet = enemyBullets.getFirstDead();
+    enemyBullet.reset(enemy.body.x, enemy.body.y);
+    enemyBullet.body.velocity.x = -300
+    game.physics.arcade.moveToObject(enemyBullet, player, 350);
+    firingTimer = game.time.now + 400;
+  }
+}
+
+function update() {
+  city.tilePosition.x -= 2;
+
+  cursors = game.input.keyboard.createCursorKeys();
+  shoot = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
+
+
+  game.physics.arcade.overlap(bullets, enemies, enemyKill, null, this);
+  game.physics.arcade.overlap(player, enemies, playerKill, null, this);
+  game.physics.arcade.overlap(player, enemyBullets, playerKill, null, this);
+  game.physics.arcade.overlap(enemies, bullets, bulletKill, null, this);
+
+  if (cursors.left.isDown)
+  {
+    player.body.velocity.x = -150;
+  }
+  else if (cursors.right.isDown)
+  {
+    player.body.velocity.x = 200;
+  }
+  else if (cursors.up.isDown)
+  {
+    player.body.velocity.y = -150;
+  }
+  else if (cursors.down.isDown)
+  {
+    player.body.velocity.y = 150;
+  }
+  else if (shoot.isDown)
+  {
+    fire();
+  }
+  else if (game.time.now > firingTimer && enemy.alive !== false)
+  {
+    enemyFire();
+  }
+  else if (player.alive === false && lives > 0)
+  {
+    player = game.add.sprite(32, game.world.height / 2, 'ship')
+    game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
+  }
+  // else if (score % 50 === 0)
+  // {
+  //   enemySpawnSpeed1 -= 10;
+  //   enemySpawnSpeed2 -= 5;
+  // }
+  else {
+    player.body.velocity.x = 50;
+  }
+}
